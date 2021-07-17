7245360a2ec8cc265b55eec4003b2f25015ca2cf
PJ-CM <pj.crespo.marichalar@gmail.com>
2021-07-17 21:32:15 +0200

    Commit.inicial.con.ciertos.paquetes.iniciales.ya.instalados

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    // Para pruebas iniciales del Laravel Debugbar...
+    $users = User::all();
+
+    // Para que se produzca la excepción de DivisionByZeroError
+    // a ser capturada por Sentry
+    //// $x = 1 / 0;
+
+    return view('welcome')->with([
+        'users' => $users,
+    ]);
+});
